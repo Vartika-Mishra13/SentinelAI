a0c60b3947b46d94db64afc7c31cccda313e422e
Vartika-Mishra13 <vartikam032@gmail.com>
2026-04-27 20:17:53 +0530

    update

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,9 +1,8 @@
-const redis = require("redis");
+const { Redis } = require("@upstash/redis");
 
-const client = redis.createClient(); // default localhost:6379
-
-client.on("error", (err) => console.log("Redis Client Error", err));
-
-client.connect(); // connect returns a promise
+const client = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 module.exports = client;
